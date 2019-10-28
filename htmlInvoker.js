c80f9d13cb4d55fbda976ff82a21a927c765fd94
andregomesp <andregomespin@gmail.com>
2019-10-28 08:17:57 -0300

    hgfghjyt

--- htmlInvoker.js
+++ htmlInvoker.js
@@ -5,6 +5,15 @@ function invokeHtml(idParent, file_url) {
         let parent = document.getElementById(idParent);
         let template = document.createElement("div");
         template.innerHTML = text.trim();
+        let scripts = template.getElementsByTagName("script");
+        for (i = 0; i < scripts.length; i++) {
+            let script = scripts[i];
+            let new_script = document.createElement("script");
+            new_script.src = script.src;
+            new_script.type = "text/javascript";
+            script.remove();
+            document.head.appendChild(new_script);
+        }
         parent.append(template);
     })
 }
