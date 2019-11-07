68dda3e984bf87879fa5db840ea65c08e166f5d4
andregomesp <andregomespin@gmail.com>
2019-11-06 23:16:27 -0300

    fosisfojfsofs

--- src/Screens/LotesVendaList/LotesVendaList.js
+++ src/Screens/LotesVendaList/LotesVendaList.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 import LoteCard from '../../Components/LoteCard/LoteCard';
-
-const lotes = []
-
+import config from "../../config.json"
 export default class LotesVendaList extends Component {
+    componentDidMount() {
+        fetch(config["serverBaseUrl"], {method: 'GET', mode: "cors"})
+        .then(r => {
+            r.json()
+            .then(json => {
+                console.log(json);
+                this.setState({lotes: json})
+            })
+        })
+    }
+
     renderLotesList = () => {
         if (this.props.lotes) {
             return this.props.lotes.map(lote => {
