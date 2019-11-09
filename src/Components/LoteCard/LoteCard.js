1b5209eb07ec8c54897a78e18ed318e0b6f5ca2e
andre gomes pinheiro <1727553>
2019-11-08 21:02:58 -0300

    jkhgdfgjkldfg

--- src/Components/LoteCard/LoteCard.js
+++ src/Components/LoteCard/LoteCard.js
@@ -6,50 +6,52 @@ import "./LoteCard.css";
 const infos = [
     {"label": "Ordem Minima", "control": ""},
     {"label": "Qtd total no lote", "control": ""},
-    {"label": "Preço Unitario", "control": ""},
     {"label": "Preço Lote", "control": ""},
 ]
 
 export default class LoteCard extends Component {
     render() {
         return (
-            <div className={"card-wrapper"}>
-                <div className={"card-first-row"}>
-                    <div className={"card-left-area"}>
-                        <div className={"image-section"}>
-
+            <>
+                <div className={"card-wrapper"}>
+                    <div className={"image-section"}>
+                                        
+                    </div>
+                    <div className={"card-first-row"}>
+                        
+                        <div className={"card-left-area"}>
+                            <div>
+                                Título
+                            </div>
+                            <div>
+                                Subtítulo
+                            </div>
                         </div>
-                        <div>
-                            Título
-                        </div>
-                        <div>
-                            Subtítulo
+                        <div className={"card-button-section"}>
+                            <img className={"card-icon"} src={clock} alt="clock"/>
+                            <div className={"days-left-wrapper"}></div>
+                            <img className={"card-icon"} src={heart} alt="heart"/>
+                            <img className={"card-icon"} src={favorite} alt="favorite"/>
                         </div>
                     </div>
-                    <div className={"card-button-section"}>
-                        <img className={"card-icon"} src={clock} alt="clock"/>
-                        <div className={"days-left-wrapper"}></div>
-                        <img className={"card-icon"} src={heart} alt="heart"/>
-                        <img className={"card-icon"} src={favorite} alt="favorite"/>
+                    <div className={"card-lower-info"}>
+                        {
+                            infos.map(info => {
+                                return (
+                                    <div className={"card-info-wrapper"}>
+                                        <div className={"card-info-label"}>{info.label}</div>
+                                        <div className="card-info-box"></div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
-                </div>
-                <div className={"card-lower-info"}>
-                    {
-                        infos.map(info => {
-                            return (
-                                <div>
-                                    {info.label}
-                                    <div className="card-info-box"></div>
-                                </div>
-                            )
-                        })
-                    }
                 </div>
                 <div className={"card-lower-buttons"}>
                     <button className={"card-yellow-button"}>Ratear Lote</button>
                     <button className={"card-yellow-button"}>Arrematar Lote</button>
                 </div>
-            </div>
+            </>
         )
     }
 }
